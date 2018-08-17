1dbe8a9df0a9f1627eb9e8cbf84537237ae7495c
viktorstrate <viktorstrate@gmail.com>
2018-08-17 16:51:17 +0200

    Initial Commit

---
+++ Time Agent/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Time Agent
+//
+//  Created by Viktor Hundahl Strate on 17/08/2018.
+//  Copyright © 2018 Viktor Hundahl Strate. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
